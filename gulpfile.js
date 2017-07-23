7519c31f6982c3d65af7fbed6d15244eb4160793
Harley Dela Cruz <harllydelacruz008@hotmail.com>
2017-07-23 22:32:20 +0800

    setup dev

--- gulpfile.js
+++ gulpfile.js
@@ -10,23 +10,16 @@
       notifier = require("node-notifier"),
       sass = require('gulp-sass'),
       imagemin = require('gulp-imagemin'),
-      source = require('vinyl-source-stream'),
-      buffer = require('vinyl-buffer'),
-      jsonfile = require('jsonfile'),
-      babel = require('gulp-babel'),
-      debowerify = require("debowerify"),
-      browserify = require('browserify'),
-      browserSync = require('browser-sync').create();
+      jsonfile = require('jsonfile');
 
-  var fonts = jsonfile.readFileSync('dev/fonts/fonts.json');
+  // var fonts = jsonfile.readFileSync('dev/fonts/fonts.json');
 
+  gulp.task('default', ['getImages', 'buildSass', 'buildSassVendor', 'watch']);
 
-  gulp.task('default', ['getFonts', 'getImages', 'buildSass', 'buildSassVendor', 'watch']);
-
-  gulp.task('getFonts', function() {
-    return gulp.src(fonts.path)
-    .pipe(gulp.dest('prod/fonts/'));
-  });
+  // gulp.task('getFonts', function() {
+  //   return gulp.src(fonts.path)
+  //   .pipe(gulp.dest('prod/fonts/'));
+  // });
 
   gulp.task('getImages', function() {
     gulp.src([
@@ -65,43 +58,9 @@
     .pipe(gulp.dest('prod/css/'));
   });
 
-  // gulp.task('buildJs', function() {
-  //   gulp.src([
-  //     'dev/scripts/*.js',
-  //     '!dev/scripts/vendor.js'
-  //   ])
-  //   .pipe(sourcemaps.init({loadMaps: true}))
-  //   .pipe(concat('main.js'))
-  //   .pipe(babel({presets: ['es2015']}).on('error', gutil.log))
-  //   .pipe(uglify().on('error', gutil.log))
-  //   .pipe(sourcemaps.write('.'))
-  //   .pipe(gulp.dest('prod/js/'));
-  // });
-  //
-  // gulp.task('buildJsVendor', function() {
-  //   var vendor = browserify('./dev/scripts/vendor.js', {
-  //     debug: false
-  //   });
-  //   vendor.transform(debowerify);
-  //   vendor.bundle()
-  //   .on('error', function (err) {
-  //     gutil.log(gutil.colors.bold.red("Browserify error (Vendor)"), err.message);
-  //     notifier.notify({title: "Browserify error (Vendor)", message: err.message });
-  //     this.emit("end");
-  //   })
-  //   .pipe(source('vendor.js'))
-  //   .pipe(buffer())
-  //   .pipe(sourcemaps.init({loadMaps: true}))
-  //   .pipe(uglify())
-  //   .pipe(sourcemaps.write('.'))
-  //   .pipe(gulp.dest('prod/js/'));
-  // });
-
   gulp.task('watch', function() {
     gulp.watch('dev/styles/main.scss', ['buildSass']);
     gulp.watch('dev/styles/vendor.scss', ['buildSassVendor']);
-    // gulp.watch('dev/scripts/main.js', ['buildJs']);
-    // gulp.watch('dev/scripts/vendor.js', ['buildJsVendor']);
-    gulp.watch(fonts.path, ['getFonts']);
+    // gulp.watch(fonts.path, ['getFonts']);
   });
 }());
